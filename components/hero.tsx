ca2d830f5d0967a96f4308974f2a3e1afcb78e34
Finit_Solutions <113910414+r0892111@users.noreply.github.com>
2025-09-11 15:46:55 +0200

    Updated hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -164,14 +164,14 @@ export function Hero() {
             <h1 className="text-4xl lg:text-6xl xl:text-7xl font-bold leading-tight" style={{ color: 'rgb(28, 44, 85)' }}>
               AI Workforces
               <br className="md:block hidden" />
-              <span className="font-semibold">that automate </span>
+              <span className="font-semibold">to automate </span>
               <br className="md:block hidden" />
               <span className="font-semibold">and scale your business.</span>
             </h1>
             <p className="text-xl font-semibold lg:text-2xl leading-relaxed max-w-2xl" style={{ color: 'rgb(28, 44, 85)' }}>
-              We build AI-powered software on top of your current systems
-              <br className="md:block hidden" />
-              which unlocks powerful leverage for your company&apos;s growth.
+              We build AI-powered workforces that eliminate inefficiencies
+              <br className="md:block hidden" /> and unlock powerful leverage
+              for your company&apos;s growth.
             </p>
           </motion.div>
 
