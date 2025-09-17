2403519a54f5304521482d13c0093333a20bed8c
Finit_Solutions <113910414+r0892111@users.noreply.github.com>
2025-09-17 14:31:03 +0200

    Updated page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,6 +6,7 @@ import { locales } from '@/i18n';
 
 export default async function HomePage({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
+}
 export function generateStaticParams() {
   return locales.map((locale) => ({
     locale: locale,
